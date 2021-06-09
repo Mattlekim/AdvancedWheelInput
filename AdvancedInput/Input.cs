5e3fb6876435e2a3a14dae4f9641e3cde7dc6606
Matt <m_brittle@hotmail.com>
2021-06-09 14:09:55 +0100

    Added input class
    Need this so that the app can work with both hat and buttons

---
+++ AdvancedInput/Input.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedInput
+{
+    public enum InputType { Button, HatUp,HatDown,HatLeft,HatRight};
+
+
+    public class Input
+    {
+        public InputType Type;
+        public int Index;
+    }
+}
